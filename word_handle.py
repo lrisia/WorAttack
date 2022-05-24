e130c9e082a3154d0ca69a3ea180d1c3e4e25358
Irisia <pochara.su@ku.th>
2022-05-25 00:02:08 +0700

    Change directory for word file

--- word_handle.py
+++ word_handle.py
@@ -1,13 +1,13 @@
 # read words_alpha.txt and return list of words
 def load_words():
-    with open('words_alpha.txt') as word_file:
+    with open('data/words_alpha.txt') as word_file:
         valid_words = set(word_file.read().split())
         word_file.close()
     return list(valid_words)
 
 def load_words_with_meaning():
     dictionary = dict()
-    with open('words_meaning.txt') as word_file:
-        word, meaning = set(word_file.read().split().split(':'))
+    with open('data/words_meaning.txt') as word_file:
+        word, meaning = set(word_file.read().split('\n').split(':'))
         dictionary[word] = meaning
     return dictionary
